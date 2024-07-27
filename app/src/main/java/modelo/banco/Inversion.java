9b527b988bea9202a437455820ce93c78add3482
Joseph <carrerasje@gmail.com>
2024-07-27 10:58:43 -0500

    Arreglando estructura de archivos y ejecucción de la aplicación

---
+++ app/src/main/java/modelo/banco/Inversion.java
@@ -0,0 +1,160 @@
+package modelo.banco;
+
+import java.time.LocalDate;
+
+/**
+ * Representa una inversión.
+ * Incluye detalles de la cuenta bancaria asociada, fechas de apertura y cierre, cantidad invertida y el interés mensual.
+ * 
+ * @autor Grupo1
+ */
+public class Inversion {
+
+    /**
+     * El código único de la inversión.
+     */
+    private static int codigo = 0;
+
+    /**
+     * La cuenta bancaria asociada a la inversión.
+     */
+    private CuentaBancaria cuentaBancaria;
+
+    /**
+     * La fecha de apertura de la inversión.
+     */
+    private LocalDate fechaApertura;
+
+    /**
+     * La cantidad invertida.
+     */
+    private double cantidad;
+
+    /**
+     * El interés mensual de la inversión.
+     */
+    private double interesMensual;
+
+    /**
+     * La fecha de cierre de la inversión.
+     */
+    private LocalDate fechaCierre;
+
+    /**
+     * Constructor de la clase Inversion.
+     *
+     * @param cuentaBancaria La cuenta bancaria asociada a la inversión.
+     * @param fechaApertura La fecha de apertura de la inversión.
+     * @param cantidad La cantidad invertida.
+     * @param interesMensual El interés mensual de la inversión.
+     * @param fechaCierre La fecha de cierre de la inversión.
+     */
+    public Inversion(CuentaBancaria cuentaBancaria, LocalDate fechaApertura, double cantidad, double interesMensual, LocalDate fechaCierre) {
+        this.cuentaBancaria = cuentaBancaria;
+        this.fechaApertura = fechaApertura;
+        this.cantidad = cantidad;
+        this.interesMensual = interesMensual;
+        this.fechaCierre = fechaCierre;
+        codigo += 1;
+    }
+
+    /**
+     * Obtiene el código de la inversión.
+     *
+     * @return El código de la inversión.
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Obtiene la cuenta bancaria asociada a la inversión.
+     *
+     * @return La cuenta bancaria asociada a la inversión.
+     */
+    public CuentaBancaria getCuentaBancaria() {
+        return cuentaBancaria;
+    }
+
+    /**
+     * Obtiene la fecha de apertura de la inversión.
+     *
+     * @return La fecha de apertura de la inversión.
+     */
+    public LocalDate getFechaApertura() {
+        return fechaApertura;
+    }
+
+    /**
+     * Establece la fecha de apertura de la inversión.
+     *
+     * @param fechaApertura La fecha de apertura de la inversión.
+     */
+    public void setFechaApertura(LocalDate fechaApertura) {
+        this.fechaApertura = fechaApertura;
+    }
+
+    /**
+     * Obtiene la cantidad invertida.
+     *
+     * @return La cantidad invertida.
+     */
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * Establece la cantidad invertida.
+     *
+     * @param cantidad La cantidad invertida.
+     */
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * Obtiene el interés mensual de la inversión.
+     *
+     * @return El interés mensual de la inversión.
+     */
+    public double getInteresMensual() {
+        return interesMensual;
+    }
+
+    /**
+     * Establece el interés mensual de la inversión.
+     *
+     * @param interesMensual El interés mensual de la inversión.
+     */
+    public void setInteresMensual(double interesMensual) {
+        this.interesMensual = interesMensual;
+    }
+
+    /**
+     * Obtiene la fecha de cierre de la inversión.
+     *
+     * @return La fecha de cierre de la inversión.
+     */
+    public LocalDate getFechaCierre() {
+        return fechaCierre;
+    }
+
+    /**
+     * Establece la fecha de cierre de la inversión.
+     *
+     * @param fechaCierre La fecha de cierre de la inversión.
+     */
+    public void setFechaCierre(LocalDate fechaCierre) {
+        this.fechaCierre = fechaCierre;
+    }
+
+    /**
+     * Devuelve una representación en cadena de la inversión.
+     *
+     * @return Una cadena que representa la inversión.
+     */
+    @Override
+    public String toString() {
+        return String.format("%-13s %-23s %-23s %-16s %-13s", codigo, cuentaBancaria.getEntidadBancaria().getEntidadBancaria(), fechaApertura, cantidad, interesMensual, fechaCierre);
+    }
+}
---
+++ app/src/main/java/modelo/cuenta/CuentaFinanciera.java
@@ -0,0 +1,190 @@
+
+package modelo.cuenta;
+
+import java.time.LocalDate;
+
+/**
+ * <h3>Clase CuentaFinanciera</h3>
+ * <p>Representa una cuenta financiera con atributos comunes a cuentas por pagar y por cobrar.</p>
+ * 
+ * @autor Grupo1
+ */
+public class CuentaFinanciera {
+    /**
+     * Código único de la cuenta.
+     */
+    protected int codigo;
+    /**
+     * Valor de la cuenta.
+     */
+    protected double valor;
+    /**
+     * Descripción de la cuenta.
+     */
+    protected String descripcion;
+    /**
+     * Fecha en la que se realizó el préstamo.
+     */
+    protected LocalDate fechaPrestamo;
+    /**
+     * Cuota de pago de la cuenta.
+     */
+    protected double cuota;
+    /**
+     * Fecha de inicio del pago de la cuenta.
+     */
+    protected LocalDate fechaInicio;
+    /**
+     * Fecha de finalización del pago de la cuenta.
+     */
+    protected LocalDate fechaFin;
+    
+    /**
+     * <h3>Constructor de la clase CuentaFinanciera</h3>
+     * <p>Inicializa una cuenta financiera con los detalles específicos.</p>
+     * 
+     * @param codigo el código de la cuenta.
+     * @param valor el valor de la cuenta.
+     * @param descripcion la descripción de la cuenta.
+     * @param fechaPrestamo la fecha en la que se realizó el préstamo.
+     * @param cuota la cuota de pago.
+     * @param fechaInicio la fecha de inicio del pago.
+     * @param fechaFin la fecha de finalización del pago.
+     */
+    public CuentaFinanciera(int codigo, double valor, String descripcion, LocalDate fechaPrestamo, double cuota, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.codigo = codigo;
+        this.valor = valor;
+        this.descripcion = descripcion;
+        this.fechaPrestamo = fechaPrestamo;
+        this.cuota = cuota;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+    
+    /**
+     * Obtiene el código de la cuenta.
+     * 
+     * @return el código de la cuenta.
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    /**
+     * Establece el código de la cuenta.
+     * 
+     * @param codigo el nuevo código de la cuenta.
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    /**
+     * Obtiene el valor de la cuenta.
+     * 
+     * @return el valor de la cuenta.
+     */
+    public double getValor() {
+        return valor;
+    }
+    
+    /**
+     * Establece el valor de la cuenta.
+     * 
+     * @param valor el nuevo valor de la cuenta.
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    /**
+     * Obtiene la descripción de la cuenta.
+     * 
+     * @return la descripción de la cuenta.
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    /**
+     * Establece la descripción de la cuenta.
+     * 
+     * @param descripcion la nueva descripción de la cuenta.
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    /**
+     * Obtiene la fecha en la que se realizó el préstamo.
+     * 
+     * @return la fecha del préstamo.
+     */
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+    
+    /**
+     * Establece la fecha en la que se realizó el préstamo.
+     * 
+     * @param fechaPrestamo la nueva fecha del préstamo.
+     */
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+    
+    /**
+     * Obtiene la cuota de pago de la cuenta.
+     * 
+     * @return la cuota de la cuenta.
+     */
+    public double getCuota() {
+        return cuota;
+    }
+    
+    /**
+     * Establece la cuota de pago de la cuenta.
+     * 
+     * @param cuota la nueva cuota de la cuenta.
+     */
+    public void setCuota(double cuota) {
+        this.cuota = cuota;
+    }
+    
+     /**
+     * Obtiene la fecha de inicio del pago de la cuenta.
+     * 
+     * @return la fecha de inicio del pago.
+     */
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+    
+    /**
+     * Establece la fecha de inicio del pago de la cuenta.
+     * 
+     * @param fechaInicio la nueva fecha de inicio del pago.
+     */
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+    
+    /**
+     * Obtiene la fecha de finalización del pago de la cuenta.
+     * 
+     * @return la fecha de finalización del pago.
+     */
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+    
+    /**
+     * Establece la fecha de finalización del pago de la cuenta.
+     * 
+     * @param fechaFin la nueva fecha de finalización del pago.
+     */
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    
+}
