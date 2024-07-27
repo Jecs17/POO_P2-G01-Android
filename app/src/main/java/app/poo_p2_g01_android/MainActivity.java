7c80562bda00b6fdc97a170aa4387915bcfd5264
JosuePach3co <pacheco400josue@gmail.com>
2024-07-26 19:37:29 -0500

    Se termino de agregar los demas eventos

--- app/src/main/java/app/poo_p2_g01_android/MainActivity.java
+++ app/src/main/java/app/poo_p2_g01_android/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
@@ -15,17 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public CardView cardViewCategoria, cardViewIngreso;
+    public CardView cardViewCategoria, cardViewIngreso, cardViewGasto, cardViewCxCobrar, cardViewCxPagar, cardViewCBancaria, cardViewInversion, cardViewPersonaBanco, cardViewReporte, cardViewProyeccionGasto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        cardViewCategoria = (CardView) findViewById(R.id.cvCategoria);
-        cardViewIngreso = (CardView) findViewById(R.id.cvIngresos);
+        cardViewCategoria = findViewById(R.id.cvCategoria);
+        cardViewIngreso = findViewById(R.id.cvIngresos);
+        cardViewGasto = findViewById(R.id.cvGastos);
+        cardViewCxCobrar = findViewById(R.id.cvCuentaxCobrar);
+        cardViewCxPagar = findViewById(R.id.cvCuentaxPagar);
+        cardViewCBancaria = findViewById(R.id.cvCuentaBancaria);
+        cardViewInversion = findViewById(R.id.cvInversiones);
+        cardViewPersonaBanco = findViewById(R.id.cvPersonaBanco);
+        cardViewReporte = findViewById(R.id.cvReportes);
+        cardViewProyeccionGasto = findViewById(R.id.cvProyeccionGastos);
+
         cardViewCategoria.setOnClickListener(this);
         cardViewIngreso.setOnClickListener(this);
+        cardViewGasto.setOnClickListener(this);
+        cardViewCxCobrar.setOnClickListener(this);
+        cardViewCxPagar.setOnClickListener(this);
+        cardViewCBancaria.setOnClickListener(this);
+        cardViewInversion.setOnClickListener(this);
+        cardViewPersonaBanco.setOnClickListener(this);
+        cardViewReporte.setOnClickListener(this);
+        cardViewProyeccionGasto.setOnClickListener(this);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -40,10 +56,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int vistaId = view.getId();
         if(vistaId == R.id.cvCategoria){
             i = new Intent(this, CategoryActivity.class);
-            startActivity(i);
         }else if(vistaId == R.id.cvIngresos) {
             i = new Intent(this, IngresoActivity.class);
-            startActivity(i);
+        }else if(vistaId == R.id.cvGastos){
+            i = new Intent(this, GastoActivity.class);
+        }else if(vistaId == R.id.cvCuentaxCobrar){
+            i = new Intent(this, CuentaxCobrarActivity.class);
+        }else if(vistaId == R.id.cvCuentaxPagar){
+            i = new Intent(this, CuentaxPagarActivity.class);
+        }else if(vistaId == R.id.cvCuentaBancaria){
+            i = new Intent(this, CuentaBancariaActivity.class);
+        }else if(vistaId == R.id.cvInversiones){
+            i = new Intent(this, InversionActivity.class);
+        }else if(vistaId == R.id.cvPersonaBanco){
+            i = new Intent(this, PersonaBancoActivity.class);
+        }else if(vistaId == R.id.cvReportes){
+            i = new Intent(this, ReporteActivity.class);
+        }else if(vistaId == R.id.cvProyeccionGastos){
+            i = new Intent(this, ProyeccionGastoActivity.class);
+        }else{
+            return;
         }
+        startActivity(i);
     }
 }
