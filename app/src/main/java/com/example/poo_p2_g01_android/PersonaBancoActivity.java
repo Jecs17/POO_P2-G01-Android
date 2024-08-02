27d888f28f4950d31dcaf331d444d6a1db3856b7
Joseph <carrerasje@gmail.com>
2024-08-01 23:09:57 -0500

    Se agregó el diseño del tableLayout y el botón para regresar a otra ventana

--- app/src/main/java/com/example/poo_p2_g01_android/PersonaBancoActivity.java
+++ app/src/main/java/com/example/poo_p2_g01_android/PersonaBancoActivity.java
@@ -20,7 +20,7 @@ public class PersonaBancoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_persona_banco);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.vistaPersonaBanco), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
